317e20287ebcabdf58bdc0d139ee6e4b2ef84237
Muhammad Hammad <143274487+MHammad-374@users.noreply.github.com>
2025-09-15 00:13:41 +0500

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -14,17 +14,94 @@ function Project() {
     {
       id: 2,
       image: Img,
-      name: "Todo App 2",
-      github: "https://github.com/MHammad-374/Todo_App",
-      prev: "https://itask-todos-manager.netlify.app/"
+      name: "Quiz Game",
+      github: "https://github.com/MHammad-374/Quiz_App",
+      prev: "https://myquizapp-123.netlify.app/"
     },
     {
       id: 3,
       image: Img,
-      name: "Todo App 3",
-      github: "https://github.com/MHammad-374/Todo_App",
-      prev: "https://itask-todos-manager.netlify.app/"
-    }
+      name: "Digital Clock",
+      github: "https://github.com/MHammad-374/Digital-Clock",
+      prev: "https://my-digital-clock-123.netlify.app/"
+    },
+    {
+      id: 4,
+      image: Img,
+      name: "Graph Plotter",
+      github: "https://github.com/MHammad-374/Graph-Plotter",
+      prev: "https://graph-plotter.netlify.app/"
+    },
+    {
+      id: 5,
+      image: Img,
+      name: "Stop Watch",
+      github: "https://github.com/MHammad-374/Stop_Watch",
+      prev: "https://stop-watch-123.netlify.app/"
+    },
+    {
+      id: 6,
+      image: Img,
+      name: "CSS Transition",
+      github: "https://github.com/MHammad-374/Transitions",
+      prev: "https://transition-io.netlify.app/"
+    },
+    {
+      id: 7,
+      image: Img,
+      name: "Landing Page",
+      github: "https://github.com/MHammad-374/Landing_Page",
+      prev: "https://landing-hi.netlify.app/"
+    },
+    {
+      id: 8,
+      image: Img,
+      name: "Hacking Terminal",
+      github: "https://github.com/MHammad-374/Hacking_Terminal",
+      prev: "https://hackingterminal.netlify.app/"
+    },
+    {
+      id: 9,
+      image: Img,
+      name: "Calculator",
+      github: "https://github.com/MHammad-374/Calculator_App",
+      prev: "https://calculator-app-abc.netlify.app/"
+    },
+    {
+      id: 10,
+      image: Img,
+      name: "Tic Tac Toe",
+      github: "https://github.com/MHammad-374/TicTacToe",
+      prev: "https://tictactoe-123.netlify.app/"
+    },
+    {
+      id: 11,
+      image: Img,
+      name: "Add To Cart",
+      github: "https://github.com/MHammad-374/AddToCart",
+      prev: "https://addtocart-func.netlify.app/"
+    },
+    {
+      id: 12,
+      image: Img,
+      name: "Text Utills",
+      github: "https://github.com/MHammad-374/Text-Utills",
+      prev: "https://text-editor-abc.netlify.app/"
+    },
+    {
+      id: 13,
+      image: Img,
+      name: "XORA Landing Page",
+      github: "https://github.com/MHammad-374/Xora_Landing_Page",
+      prev: "https://x-o-r-a-landing-page.netlify.app/"
+    },
+    {
+      id: 14,
+      image: Img,
+      name: "AI Chatbot",
+      github: "https://github.com/MHammad-374/Chatboot",
+      prev: "https://ai-chatboot.netlify.app/"
+    },
   ]
 
   const openNewTab = (url) => {
